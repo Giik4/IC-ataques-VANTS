4d2a9dcbc3d89562f0656e5938a370abe4ce06e9
Giovanni Siervo <ghmlsiervo@gmail.com>
2023-09-19 01:18:09 -0300

    atualizando código e adicionando arquivos

--- ataque.py
+++ ataque.py
@@ -11,7 +11,7 @@ print("Heartbeat from system (system %u component %u)" %
 
 
 the_connection.mav.send(mavutil.mavlink.MAVLink_set_position_target_global_int_message(10, the_connection.target_system,
-                        the_connection.target_component, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, int(0b110111111000), int(-35.35787727 * 10 ** 7), int(149.16975067 * 10 ** 7), 10, 0, 0, 0, 0, 0, 0, 0, 0))
+                        the_connection.target_component, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, int(0b110111111000), int(-35.36384566 * 10 ** 7), int(149.16480758 * 10 ** 7), 10, 0, 0, 0, 0, 0, 0, 0, 0))
 
 msg = the_connection.recv_match(type= 'COMMAND_ACK', blocking=True)
 print(msg)
--- captura.py
+++ captura.py
@@ -1,9 +1,4 @@
 from pymavlink import mavutil
-import csv
-
-with open('dataAtaque.csv', 'w', newline='') as csvfile:
-    campos_head =['tempo','latitude','longitude','altitude']
-    writer = csv.DictWriter(csvfile, fieldnames=campos_head)
 
 the_connection = mavutil.mavlink_connection('udpin:localhost:14551')
 
@@ -11,19 +6,8 @@ the_connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" %
       (the_connection.target_system, the_connection.target_component))
 
-writer.writerheader()
 
-while 1:
-    try: 
-        altitude = the_connection.messages['GLOBAL_POSITION_INT'].alt
-        latitude = the_connection.messages['GLOBAL_POSITION_INT'].lat
-        longitude = the_connection.messages['GLOBAL_POSITION_INT'].lon
-        timestamp = the_connection.messages['GLOBAL_POSITION_INT'].time_boot_ms
-        print('altitude: ',altitude)
-        print('latitude: ',latitude)
-        print('longitude: ',longitude)
-        print('tempo ',timestamp)
-        writer.writerow({'tempo': timestamp,'latitude': latitude, 'longitude':longitude, 'altitude':altitude} )
-
-    except:
-        print('no GLOBAL_POSITION_INT message received')
+while 1: 
+    msg = the_connection.recv_match(
+        type='GLOBAL_POSITION_INT', blocking=True)
+    print(msg) 
